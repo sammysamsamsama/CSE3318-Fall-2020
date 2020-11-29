8bbbe24ad3cd61342c1aec39098c065b2d9b0e72
Samson <45101270+sammysamsamsama@users.noreply.github.com>
2020-11-28 22:11:12 -0600

    from clion

---
+++ lab4fall20/cmake-build-debug/CMakeFiles/lab4fall20.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4fall20.dir/lab4fall20.c.obj"
+  "lab4fall20.exe"
+  "lab4fall20.exe.manifest"
+  "lab4fall20.pdb"
+  "liblab4fall20.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab4fall20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
